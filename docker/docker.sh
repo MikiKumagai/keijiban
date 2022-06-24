e46fec320fc1f55372cbb1f145a821c8fd11acd3
YukiKumagai0619 <yuki.kumagai@skewers.me>
2022-06-25 08:55:30 +0900

    Run on docker

---
+++ docker/docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+NAME=keijiban
+IMAGE_NAME=${NAME}-image
+CONTAINER_NAME=${NAME}-container
+
+docker build -t ${IMAGE_NAME}  -f ${SCRIPT_DIR}/Dockerfile .
+docker run --rm \
+    --name ${CONTAINER_NAME} \
+    -p 80:80 \
+    -v $(pwd):/var/www/html \
+    ${IMAGE_NAME}
