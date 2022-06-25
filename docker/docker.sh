6ca6cdccc4b191f0267363411b0fc292f60d0662
YukiKumagai0619 <yuki.kumagai@skewers.me>
2022-06-25 09:01:54 +0900

    Changed keijiban.html path

--- docker/docker.sh
+++ docker/docker.sh
@@ -11,5 +11,5 @@ docker build -t ${IMAGE_NAME}  -f ${SCRIPT_DIR}/Dockerfile .
 docker run --rm \
     --name ${CONTAINER_NAME} \
     -p 80:80 \
-    -v $(pwd):/var/www/html \
+    -v $(pwd)/keijiban:/var/www/html/keijiban \
     ${IMAGE_NAME}
