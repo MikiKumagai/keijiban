e23f0275cd28421488b067bd394ba720601b5b01
YukiKumagai0619 <yuki.kumagai@skewers.me>
2022-06-26 15:52:03 +0900

    Fix docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -7,6 +7,9 @@ NAME=keijiban
 IMAGE_NAME=${NAME}-image
 CONTAINER_NAME=${NAME}-container
 
+DATA_FILES="${SCRIPT_DIR}/../keijiban.html ${SCRIPT_DIR}/../log.txt"
+touch ${DATA_FILES} && chmod 666 ${DATA_FILES}
+
 docker build -t ${IMAGE_NAME} -f ${SCRIPT_DIR}/Dockerfile .
 docker run --rm \
     --name ${CONTAINER_NAME} \
