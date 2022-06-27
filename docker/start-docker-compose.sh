a5ed6ce0b3fac3630b1854304dea168b226bd0bb
YukiKumagai0619 <yuki.kumagai@skewers.me>
2022-06-27 20:24:55 +0900

    Moved log file

--- docker/start-docker-compose.sh
+++ docker/start-docker-compose.sh
@@ -4,7 +4,8 @@ set -eux
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-DATA_FILES="${SCRIPT_DIR}/../keijiban.html ${SCRIPT_DIR}/../log.txt"
+DATA_FILES="${SCRIPT_DIR}/../keijiban.html ${SCRIPT_DIR}/../logs/log.txt"
+mkdir -p ${SCRIPT_DIR}/../logs
 touch ${DATA_FILES} && chmod 666 ${DATA_FILES}
 
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up --build -d
