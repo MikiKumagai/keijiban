a54006aab4e5c08e9b8331468c594c231807732e
MikiKumagai <108138295+MikiKumagai@users.noreply.github.com>
2022-10-18 14:33:49 +0900

    クリエイトテーブルを消去

--- htdocs/plan-insert.php
+++ htdocs/plan-insert.php
@@ -10,36 +10,11 @@
             $sql = $db->prepare('INSERT INTO plan(theme, category, comment, date) VALUES(:theme, :category, :comment, NOW()) ');
             $sql->execute(array(':theme' => $theme, ':category' => $category, ':comment' => $comment));
        
-            echo '初期エントリー';
-            echo '<br>';
-            echo '<form method="POST" action="vote-insert2.php">
-            <input type="text" name="entry" size="40"><br>
-            <input type="text" name="entry" size="40"><br>
-            <input type="text" name="entry" size="40"><br>
-            <input type="submit" value="送信">
-            </form><br>';
+            echo 'ランキングが作成されました';
 
             $id = $db -> lastinsertID(); 
 
-            $db2 = new PDO('mysql:dbname=keijiban;host=localhost;charset=utf8','kmmk','uwfyzcyr');
-                        
-            $sql2 = 'CREATE TABLE '.$id.'BBS(
-                NUM MEDIUMINT NOT NULL AUTO_INCREMENT,
-                contents varchar(150),
-                date datetime,
-                PRIMARY KEY (NUM))';
-            $db2->query($sql2);
-
-
-            $db3 = new PDO('mysql:dbname=tohyo;host=localhost;charset=utf8','kmmk','uwfyzcyr');
-                        
-            $sql3 = 'CREATE TABLE '.$id.'vote(
-                name varchar(20),
-                count int default 1,
-                id varchar(13))';
-            $db3->query($sql3);
-
-            } catch(PDOException $e){
+ } catch(PDOException $e){
             echo 'エラーが発生しました' . $e->getMessage();
         }
 
