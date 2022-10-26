af4d97a75fb497be66f913f62b2466ebc425a80b
MikiKumagai <108138295+MikiKumagai@users.noreply.github.com>
2022-10-26 13:21:24 +0900

    URLパラメータ

--- htdocs/plan-insert.php
+++ htdocs/plan-insert.php
@@ -17,8 +17,8 @@
             $category = $_POST['category'];
             $comment = $_POST['comment'];
                         
-            $sql = $db->prepare('INSERT INTO plan(theme, category, comment, date, thread_id) VALUES(:theme, :category, :comment, NOW(), :thread_id) ');
-            $sql->execute(array(':theme' => $theme, ':category' => $category, ':comment' => $comment, ':thread_id' => uniqid() ));
+            $sql = $db->prepare('INSERT INTO plan(theme, category, comment, date) VALUES(:theme, :category, :comment, NOW()) ');
+            $sql->execute(array(':theme' => $theme, ':category' => $category, ':comment' => $comment ));
        
             $sql2 = "SELECT * FROM plan";
             $sth = $db -> query($sql2);
@@ -31,7 +31,7 @@
 ?>
 
 ランキングが作成されました<br>
-<a href="http://localhost/keijiban.html?id=<?php echo $count; ?>">作成したランキングページを表示する</a><br>
+<a href="http://localhost/keijiban.html?thread_id=<?php echo $count; ?>">作成したランキングページを表示する</a><br>
 <a href="top.html">戻る</a>
 
 </body>
