efefe7c8575d284e79b88866dce2ad204406aaa7
MikiKumagai <108138295+MikiKumagai@users.noreply.github.com>
2022-11-02 14:42:04 +0900

    調整

--- htdocs/vote-insert.php
+++ htdocs/vote-insert.php
@@ -3,8 +3,15 @@
     if(!empty($_POST['entry'])){
         try{
             $db = new PDO('mysql:dbname=ranking;host=localhost;charset=utf8','kmmk','uwfyzcyr');
-            $sql = $db->prepare('INSERT INTO vote SET name = ?, count = 1, id = ?');
-            $sql->execute(array($_POST['entry'], uniqid()));
+            
+            $Data = $db->prepare('SELECT * FROM plan WHERE thread_id=?');
+            $Data->execute(array($_REQUEST['thread_id']));
+            $hyouji = $Data->fetch();
+            
+            $thread_id = $hyouji['thread_id'];
+
+            $sql = $db->prepare('INSERT INTO vote SET name = ?, count = 1, id = ?, thread_id = ?');
+            $sql->execute(array($_POST['entry'], uniqid(), $thread_id));
             echo '投票完了';
             echo '<br>';
             echo '<a href="tohyo.html">戻る</a>';
--- htdocs/vote-update.php
+++ htdocs/vote-update.php
@@ -2,7 +2,8 @@
     
 if(!empty($_POST['vote'])){
         try{
-            $db = new PDO('mysql:dbname=tohyo;host=localhost;charset=utf8','kmmk','uwfyzcyr');
+            $db = new PDO('mysql:dbname=ranking;host=localhost;charset=utf8','kmmk','uwfyzcyr');
+            
             $sql = $db->prepare('UPDATE vote SET count = count+1 WHERE id = ?');
             $sql->execute(array($_POST['vote']));
             echo '投票完了';
