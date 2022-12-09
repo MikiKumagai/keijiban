09a4a80c709ecda0e17fd0441d708ffc42e88568
MikiKumagai <108138295+MikiKumagai@users.noreply.github.com>
2022-12-09 15:08:52 +0900

    toppage php

---
+++ htdocs/to_pl_s.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>keijiban.php</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./color.css">
+    </head>
+<body>
+    
+    <?php  
+
+        try {
+            $pdo = new PDO(
+                'mysql:host=localhost;dbname=ranking;','kmmk','uwfyzcyr',
+                [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
+            $sql = "SELECT * FROM plan ORDER BY thread_id DESC";
+            $pdo -> query($sql);
+
+            foreach ($pdo -> query($sql) as $row) {
+            echo '<p class="text-dark lh-base">';
+            echo "<a href=\"KEIJIBAN.html?thread_id={$row['thread_id']}\">三大{$row['theme']} {$row['date']}</a>";
+            echo '</p>';
+            }
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+
+        } finally {
+            $pdo = null;
+        }
+
+    ?>
+
+</body>
+</html>
