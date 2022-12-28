7a5beb1427708305537f7f986574b2f94b1bb3f0
MikiKumagai <108138295+MikiKumagai@users.noreply.github.com>
2022-12-28 16:38:05 +0900

    w

--- htdocs/plan-insert.php
+++ htdocs/plan-insert.php
@@ -5,9 +5,13 @@
     <title>
         企画｜三大○○
     </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./color.css">
 </head>
 
-<body>
+<body class="bg-light">
 <?php
     
         try{
@@ -17,7 +21,7 @@
             $category = $_POST['category'];
             $comment = $_POST['comment'];
                         
-            $sql = $db->prepare('INSERT INTO plan(theme, category, comment, date) VALUES(:theme, :category, :comment, NOW()) ');
+            $sql = $db->prepare('INSERT INTO plan(theme, category, comment, date, active) VALUES(:theme, :category, :comment, NOW(), NOW()) ');
             $sql->execute(array(':theme' => $theme, ':category' => $category, ':comment' => $comment ));
        
             $sql2 = "SELECT * FROM plan";
@@ -29,10 +33,10 @@
         }
 
 ?>
-
-ランキングが作成されました<br>
-<a href="http://localhost/keijiban.html?thread_id=<?php echo $count; ?>">作成したランキングページを表示する</a><br>
-<a href="top.html">戻る</a>
+<div class='text-center m-5'>
+    スレッドが作成されました<br>
+    <a crass="btn btn-primary" href="keijiban.html?thread_id=<?php echo $count; ?>">作成したページを表示する</a>
+</div>
 
 </body>
 
