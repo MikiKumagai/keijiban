75d1760d7a454ab9320be69722d5bf57266fb967
MikiKumagai <m.kumagai689@gmail.com>
2023-01-11 14:45:59 +0900

    edit dockerfile

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,1 +1,14 @@
-FROM php:8.1.7-apache
+FROM php:apache
+
+RUN apt update && \
+    apt install unzip
+
+RUN curl \
+        -o /tmp/bootstrap-5.2.2-dist.zip \
+        -vL https://github.com/twbs/bootstrap/releases/download/v5.2.2/bootstrap-5.2.2-dist.zip && \
+    unzip \
+        /tmp/bootstrap-5.2.2-dist.zip \
+        -d /var/www/html && \
+    rm /tmp/bootstrap-5.2.2-dist.zip
+
+RUN docker-php-ext-install pdo_mysql
