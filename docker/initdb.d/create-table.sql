d42a808cbf61d308c1237511b67679638a7a0b62
MikiKumagai <m.kumagai689@gmail.com>
2023-01-11 17:33:13 +0900

    Add creating initial db script

--- docker/initdb.d/create-table.sql
+++ docker/initdb.d/create-table.sql
@@ -1,5 +1,31 @@
-CREATE TABLE IF NOT EXISTS `keijiban`.`tohyo` (
-   `id`     INT NOT NULL,
-   `choice` VARCHAR(100) NOT NULL,
-   `count`  INT NOT NULL
+CREATE USER apache_webserver IDENTIFIED BY 'uwfyzcyr';
+GRANT ALL PRIVILEGES ON * . * TO apache_webserver;
+FLUSH PRIVILEGES;
+
+CREATE DATABASE ranking;
+
+CREATE TABLE IF NOT EXISTS ranking.plan(
+   `theme`     VARCHAR(20),
+   `category`  INT,
+   `comment`   VARCHAR(300),
+   `date`      DATETIME,
+   `active`    DATETIME,
+   `thread_id` INT AUTO_INCREMENT,
+   PRIMARY KEY (thread_id)
+);
+CREATE TABLE IF NOT EXISTS  ranking.vote(
+   `name`      VARCHAR(20),
+   `count`     INT,
+   `id`        VARCHAR(13),
+   `thread_id` INT
+);
+CREATE TABLE IF NOT EXISTS  ranking.user(
+   `IP`        VARCHAR(15),
+   `thread_id` INT
+);
+CREATE TABLE IF NOT EXISTS  ranking.bbs(
+   `contents`  VARCHAR(500),
+   `date`      DATETIME,
+   `thread_id` INT,
+   `name`      VARCHAR(20)
 );
