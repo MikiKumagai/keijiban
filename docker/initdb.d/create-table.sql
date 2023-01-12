5dd65e90de94e3ff17a7266ed788bf378bc6ba89
MikiKumagai <m.kumagai689@gmail.com>
2023-01-12 17:42:22 +0900

    文字数カウント

--- docker/initdb.d/create-table.sql
+++ docker/initdb.d/create-table.sql
@@ -5,16 +5,16 @@ FLUSH PRIVILEGES;
 CREATE DATABASE ranking;
 
 CREATE TABLE IF NOT EXISTS ranking.plan(
-   `theme`     VARCHAR(20),
+   `theme`     VARCHAR(30),
    `category`  INT,
-   `comment`   VARCHAR(300),
+   `comment`   VARCHAR(200),
    `date`      DATETIME,
    `active`    DATETIME,
    `thread_id` INT AUTO_INCREMENT,
    PRIMARY KEY (thread_id)
 );
 CREATE TABLE IF NOT EXISTS  ranking.vote(
-   `name`      VARCHAR(20),
+   `name`      VARCHAR(30),
    `count`     INT,
    `id`        VARCHAR(13),
    `thread_id` INT
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS  ranking.user(
    `thread_id` INT
 );
 CREATE TABLE IF NOT EXISTS  ranking.bbs(
-   `contents`  VARCHAR(500),
+   `contents`  VARCHAR(200),
    `date`      DATETIME,
    `thread_id` INT,
-   `name`      VARCHAR(20)
+   `name`      VARCHAR(30)
 );
