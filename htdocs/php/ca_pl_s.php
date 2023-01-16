62850debe994a4875344b6eb1fbb1106c314229c
MikiKumagai <m.kumagai689@gmail.com>
2023-01-16 16:37:06 +0900

    categoryIDを変数に入れるやり方修正

--- htdocs/php/ca_pl_s.php
+++ htdocs/php/ca_pl_s.php
@@ -13,23 +13,22 @@
 
         try {
             require_once("DB_connect.php");
-            require_once("category.php");
 
-            $stmt = "select * from plan WHERE category={$category} order by thread_id DESC";
-            $db -> query($stmt);
-    
-            foreach ($db -> query($stmt) as $row) {
-            echo "<a id=active href=\"KEIJIBAN.html?thread_id={$row['thread_id']}\">";
-            echo '<p class="px-4"><font size=5>';
-            echo "三大{$row['theme']}";
-            echo '</font>';
-            echo '<p class="px-4" style="text-align:right"><font size=2>';
-            echo " {$row['active']} 更新";
-            echo '</font>';
-            echo '</a>';
-            echo '<hr>';
-              }
-        
+            $category = $_REQUEST['id'];
+            $sql = "select * from plan WHERE category={$category} order by active DESC";
+            $db -> query($sql);
+
+                foreach ($db -> query($sql) as $row) {
+                echo "<a id=active href=\"KEIJIBAN.html?thread_id={$row['thread_id']}\">";
+                echo '<p class="px-4"><font size=5>';
+                echo "三大{$row['theme']}";
+                echo '</font>';
+                echo '<p class="px-4" style="text-align:right"><font size=2>';
+                echo " {$row['active']} 更新";
+                echo '</font>';
+                echo '</a>';
+                echo '<hr>';
+                }
         } catch (PDOException $e) {
             echo $e->getMessage();
         
